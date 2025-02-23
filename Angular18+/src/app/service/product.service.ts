10e2f8a6697353e04594d8735d3f1b5546d4cd78
Yarod Bonilla <yarold_br@outlook.es>
2025-02-23 10:28:30 -0500

    ngrok

--- Angular18+/src/app/service/product.service.ts
+++ Angular18+/src/app/service/product.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../enviroment/enviroment';
 import { IProduct } from '../model/product';
 
 
@@ -8,35 +9,34 @@ import { IProduct } from '../model/product';
   providedIn: 'root'
 })
 export class ProductService {
-  private apiUrl = 'http://localhost:8080/product';
-
+  private apiUrl = `${environment.apiUrl}/product`;
+  private headers = new HttpHeaders({ 'ngrok-skip-browser-warning': 'true' });
   constructor(private http: HttpClient) { }
 
   getAllProducts(): Observable<IProduct[]> {
-    return this.http.get<IProduct[]>(this.apiUrl);
+    return this.http.get<IProduct[]>(this.apiUrl, { headers: this.headers });
   }
 
   getByIdProduct(id: string): Observable<IProduct> {
-    return this.http.get<IProduct>(`${this.apiUrl}/${id}`);
+    return this.http.get<IProduct>(`${this.apiUrl}/${id}`, { headers: this.headers });
   }
 
   createProduct(product: Omit<IProduct, 'id' | 'img'>): Observable<IProduct> {
-    return this.http.post<IProduct>(this.apiUrl, product);
+    return this.http.post<IProduct>(this.apiUrl, product, { headers: this.headers });
   }
 
   updateProduct(id: string, product: Partial<IProduct>): Observable<IProduct> {
-    return this.http.put<IProduct>(`${this.apiUrl}/${id}`, product);
+    return this.http.put<IProduct>(`${this.apiUrl}/${id}`, product, { headers: this.headers });
   }
 
   uploadImage(file: File, productId: string): Observable<any> {
     const formData = new FormData();
     formData.append('image', file);
 
-    return this.http.post(`${this.apiUrl}/${productId}/upload-image`, formData);
+    return this.http.post(`${this.apiUrl}/${productId}/upload-image`, formData, { headers: this.headers });
   }
 
   deleteProduct(id: string): Observable<string> {
-    console.log(Observable)
-    return this.http.delete(`${this.apiUrl}/${id}`, { responseType: 'text' });
+    return this.http.delete(`${this.apiUrl}/${id}`, { headers: this.headers, responseType: 'text' });
   }
 }
